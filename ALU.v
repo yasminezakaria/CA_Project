5a1d25d68859106d165b32d1bed57ca3c0da86ac
yasminezakaria <yasminezakaria@outlook.com>
2018-05-04 18:49:24 +0200

    ALU done

--- ALU.v
+++ ALU.v
@@ -1,30 +1,23 @@
 module ALU( input [31:0] A,B,addressSignExtend,pc,
- input [2:0] Sel,
+ input [5:0] Sel,
  output [31:0] ALU_Out,
  output reg zeroFlag,
  output reg [31:0] addResult);
 
 reg [31:0] Res;
 assign ALU_Out = Res;
-
-
 always @(*)
 begin
 addResult = (addressSignExtend << 2) + pc;
 case(Sel)
- 3'b000: Res = A & B; // And
- 3'b001: Res = A + B; // Add
- 3'b010: Res = A | B; // Or
- 3'b011: Res = A - B; // subtract
- 3'b100: Res = (A<B)?8'b00000001:8'b0000000; // Set on less than
- 3'b101: Res = ~(A | B); // NOR
-
+ 6'b000000: begin Res = A + B; zeroFlag = (A==B)?1'b1:1'b0; end // Add
+ 6'b000001: begin Res = A - B; zeroFlag = (A==B)?1'b1:1'b0; end// subtract
+ 6'b000010: begin Res = A & B; zeroFlag = (A==B)?1'b1:1'b0; end// And
+ 6'b000011: begin Res = A | B; zeroFlag = (A==B)?1'b1:1'b0; end// Or
+ 6'b000100: begin Res = A << B; zeroFlag = (A==B)?1'b1:1'b0; end// shift left
+ 6'b000101:  begin Res = A >> B; zeroFlag = (A==B)? 1'b1:1'b0; end // shift right
+ 6'b000110:	begin Res = (A < B) ? 1 : 0; zeroFlag = (A==B)?1'b1:1'b0; end //slt
+ 6'b000111:	begin Res = (A < B) ? 1 : 0; zeroFlag = (A==B)?1'b1:1'b0; end //sltu
  endcase
  end
-
-always @(*)
- begin
-    zeroFlag = (A==B)?1'b1:1'b0;
-end
 endmodule
-
